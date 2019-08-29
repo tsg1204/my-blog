2eeb74f3a75b74e836b6643877923a07403493fa
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2019-08-28 20:16:22 -0400

    first commit

---
+++ src/build/precache-manifest.b0665680ffda2f8cfb4849f68a980218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9900637611597ab87db70039357cc55f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ff1237ec6c07114631",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "ecef145b20f43aaa6d94",
+    "url": "/static/js/2.db896202.chunk.js"
+  },
+  {
+    "revision": "82ff1237ec6c07114631",
+    "url": "/static/js/main.cdd2f822.chunk.js"
+  },
+  {
+    "revision": "eeab855a9420d0df9e27",
+    "url": "/static/js/runtime~main.a3235f1f.js"
+  }
+]);
